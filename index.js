1d9200e0e11ef75e3a6454dbedd7d8d06384243c
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-18 20:13:59 -0400

    adds routes, hlemt & morgan

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const helmet = require('helmet')
+const morgan = require('morgan')
 const Joi = require('joi')
 const express = resquire('express')
 const app = express()
+
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(helmet())
+
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'))
+    console.log('Using Morgan')
+}
 
 const port = process.env.PORT || 5000;
 app.list(port, () => console.log(`Listening on port ${port}`))
