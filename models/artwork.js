38d7eb0f5baaa0a6f40c620cf80f3e02c08c1fc6
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-25 12:59:34 -0400

    adds artwork model

---
+++ models/artwork.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+// give name
+mongoose.connect('mongodb://localhost/artworks')
+    .then(() => console.log('Connected to MongoDB')
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+const artworkSchema = new mongoose.Schema({
+    artist: String,
+    title: String,
+    year: Integer,
+    medium: String,
+    price: Integer,
+    ntoes: String,
+    tags: [ String ],
+    dateAdded: { type: Date, default: Date.now },
+    lastUpdated: { type: Date, default: Date.now }
+});
+
+async function createArtwork(){
+    const Artwork = mongoose.model('Artwork', artworkSchema);
+    const artwork = new Artwork();
+
+    const result = await artwork.save();
+    
+}
