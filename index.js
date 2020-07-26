6466dadbe464ff51f7d1e1cfd6e5acef33203f85
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-26 19:44:27 -0400

    add db connection

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(helmet())
 
+// db
+// give name
+mongoose.connect('mongodb://localhost/artinv')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Could not connect to MongoDB', err));
+
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'))
     console.log('Using Morgan')
