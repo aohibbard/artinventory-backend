8b19d37ee833dc58253f50e8cb84a2c8e22ebdcc
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-26 19:52:14 -0400

    creates artwork schema w/ validations

--- models/artwork.js
+++ models/artwork.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose')
+const Joi = require('joi')
 
-// give name
-mongoose.connect('mongodb://localhost/artworks')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log('Could not connect to MongoDB', err));
 
-const artworkSchema = new mongoose.Schema({
-    artist: { type: String, required: true},
-    title: { type: String, required: true },
+const Artwork = mongoose.model('Artwork', new mongoose.Schema({
+    artist: { 
+        type: String,
+        required: true,
+        minLength: 3},
+    title: {
+        type: String,
+        required: true,
+        minLength: 2 },
     year: { type: Number, required: true },
     medium: String,
+    dimensions: String,
     price: Number,
     notes: String,
     tags: [ String ],
     dateAdded: { type: Date, default: Date.now },
     lastUpdated: { type: Date, default: Date.now }
-});
+}));
 
-async function createArtwork(){
-    const Artwork = mongoose.model('Artwork', artworkSchema);
-    const artwork = new Artwork();
-
-    const result = await artwork.save();
-
+function validateArtwork(artwork){
+    const schema = {
+        artist: Joi.string.min(3).required(),
+        title: Joi.string.min(2).required(),
+        year: Joi.number.required(),
+        medium: Joi.string(),
+        dimensions: Joi.string(),
+        tags: Joi.array().items({ a: Joi.string() }),
+        dateAdded: Joi.date(),
+        lastUpdated: Joi.date()
+    }
 }
+
+
+exports.Artwork = Artwork;
+exports.validate = validateArtwork;
