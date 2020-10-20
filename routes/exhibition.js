f3ad8d02cd04b273290b5aa1e5c2af5310bcc219
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-10-20 18:24:30 +0100

    post route for exhibition

--- routes/exhibition.js
+++ routes/exhibition.js
@@ -8,9 +8,31 @@ Fawn.init(mongoose);
 
 // INDEX
 router.get('/', async (req, res) => {
-    const exhibition = await Exhibition.find.sort('lastUpdated');
+    const exhibitions = await Exhibition.find.sort('lastUpdated');
     res.send(exhibitions);
 });
 
+router.get('/:id', async(req, res) => {
+    const exhibition = await Exhibition.findById(req.params.id);
+    if (!exhibition) return res.status(404).send('Exhibition does not exist');
+    res.send(exhibition)
+})
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let exhibition = new Exhibition({
+        name: req.body.name,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        venue: req.body.venue,
+        city: req.body.city,
+        dateAdded: req.body.dateAdded,
+        dateUpdated: Date()
+    })
+    exhibition = await exhibition.save();
+    res.send(exhibition);
+})
 
 module.exports = router;
